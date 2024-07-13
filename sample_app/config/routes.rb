90f2630cab5aeea25e943f2b61f5cb936afd83f2
varshigak <varshigak@gmail.com>
2024-07-12 22:35:46 -0400

    Finish static pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +12,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'application#hello'
 end
